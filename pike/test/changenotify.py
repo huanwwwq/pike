499f1f1e5eff039e02abc598d6f25faba881ae17
Qiang Wang <Wang.Qiang3@emc.com>
2019-03-05 15:55:17 +0800

    some local test

--- pike/test/changenotify.py
+++ pike/test/changenotify.py
@@ -37,6 +37,8 @@
 import pike.model
 import pike.smb2
 import pike.test
+import datetime
+import time
 
 class ChangeNotifyTest(pike.test.PikeTest):
     def test_change_notify_file_name(self):
@@ -75,5 +77,55 @@ class ChangeNotifyTest(pike.test.PikeTest):
         self.assertEqual(len(result.notifications), 1)
         self.assertEqual(result.notifications[0].filename, filename)
         self.assertEqual(result.notifications[0].action, pike.smb2.SMB2_ACTION_ADDED)
+        
+        chan.close(root)
+
+    def test_change_notify_creation(self):
+        filename = "change_notify.txt"
+        chan, tree = self.tree_connect()
+
+        # connect to the root of the share
+        root = chan.create(tree,
+                           '',
+                           access=pike.smb2.GENERIC_READ,
+                           options=pike.smb2.FILE_DIRECTORY_FILE,
+                           share=pike.smb2.FILE_SHARE_READ).result()
+
+        # build a change notify request
+        smb_req = chan.request(obj=root)
+        notify_req = pike.smb2.ChangeNotifyRequest(smb_req)
+        notify_req.file_id = root.file_id
+        notify_req.completion_filter = pike.smb2.SMB2_NOTIFY_CHANGE_CREATION
+        # send the request async
+        futures = chan.connection.submit(smb_req.parent)
+
+        # create a file on the share to trigger the notification
+           
+
+        file = chan.create(tree,
+                           filename,
+                           access=pike.smb2.GENERIC_READ | pike.smb2.GENERIC_WRITE | pike.smb2.DELETE,
+                           share=pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE,
+                           options=pike.smb2.FILE_DELETE_ON_CLOSE,
+                           disposition=pike.smb2.FILE_SUPERSEDE).result()
+        
+        now = pike.nttime.NtTime(datetime.datetime.now())
+        import pdb
+        pdb.set_trace()
+        time.sleep(5)
+        with chan.set_file_info(file, pike.smb2.FileBasicInformation) as file_info:
+            file_info.creation_time = now
+
+
+        chan.close(file)
+
+        
+        # collect the change notify response
+        result = futures[0].result()[0]
+
+        # expect one notification, for the file add
+        self.assertEqual(len(result.notifications), 1)
+        self.assertEqual(result.notifications[0].filename, filename)
+        #self.assertEqual(result.notifications[0].action, pike.smb2.SMB2_ACTION_ADDED)
         
         chan.close(root)
